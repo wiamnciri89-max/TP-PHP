b1ecf3ef9d2f596b0655d73700b45ac4a2699344
Nciri Wiam <wiamnciri89@gmail.com>
2026-09-12 23:52:37 +0200

    Ex6

---
+++ ex6.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="post">
+        <label>Nom du produit : </label>
+        <input type="text" name="produit">
+        <br>
+        
+        <label>Prix :</label>
+        <input type="number" name="prix">
+        <br>
+
+        <label>Quantité :</label>
+        <input type="number" name="quantite">
+        <br>
+
+        <button type="submit">Valider</button>
+        
+    </form>
+</body>
+</html>
