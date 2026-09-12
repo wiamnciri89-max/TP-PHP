4a5fe77a68f03547be780979c383559f651257c1
Nciri Wiam <wiamnciri89@gmail.com>
2026-09-13 00:24:02 +0200

    Ajouter le code de ex6

--- ex6.php
+++ ex6.php
@@ -6,6 +6,18 @@
     <title>Document</title>
 </head>
 <body>
+    <?php
+    if (isset($_POST["produit"]) && isset($_POST["prix"]) && isset($_POST["quantite"])) {
+        $produit = $_POST["produit"];
+        $prix = $_POST["prix"];
+        $quantite = $_POST["quantite"];
+
+        $prixtotal = $prix * $quantite;
+
+        echo "Commande : $quantite x $produit<br>";
+        echo "Prix total : $prixtotal €<br>";
+    }
+    ?>
     <form method="post">
         <label>Nom du produit : </label>
         <input type="text" name="produit">
@@ -20,7 +32,7 @@
         <br>
 
         <button type="submit">Valider</button>
-        
+
     </form>
 </body>
 </html>
