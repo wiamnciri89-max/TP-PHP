857059e1e4d1fba663a0b2719a28ace56aa01560
Wiam Nciri <wiamnciri89@gmail.com>
2026-09-14 12:42:44 +0200

    EX8.3

---
+++ produits.php
@@ -0,0 +1,12 @@
+<?php
+require "connexion.php";
+
+$sql = "SELECT * FROM produit";
+$stmt = $pdo->query($sql);
+$produits = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($produits as $produit) {
+    echo $produit['nom'] . " - " . $produit['prix'] . " €\n";
+}
+
+?>
