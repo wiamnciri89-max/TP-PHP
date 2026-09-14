7e3568a1fd79f46b734ccf801b0e2b6e25267599
Wiam Nciri <wiamnciri89@gmail.com>
2026-09-14 12:08:20 +0200

    EX8.2

---
+++ connexion.php
@@ -0,0 +1,25 @@
+<?php
+
+$host = "localhost";
+$dbname = "boutique";
+$user = "root";
+$password = "";
+
+try {
+
+    $pdo = new PDO(
+        "mysql:host=$host;dbname=$dbname;charset=utf8mb4",
+        $user,
+        $password
+    );
+
+    $pdo->setAttribute(
+        PDO::ATTR_ERRMODE,
+        PDO::ERRMODE_EXCEPTION
+    );
+
+} catch (PDOException $e) {
+
+    die("Erreur de connexion à la base de données");
+
+}
